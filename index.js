0a2e118a0be861a44a91fadc2a6ab926fc3e6b58
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-04-22 12:07:06 -0600

    second

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 let count = 0
+const element = document.getElementById('counter')
 
 function increase(){
   count++
-  console.log(count)  
+  element.innerText = count 
+}
+
+function decrease(){
+    count--
+    element.innerText = count 
+}
+
+function reset(){
+    count = 0
+    element.innerHTML = `<mark>${count}</mark>`
+}
+
+function slectTheme(theme){
+    document.getElementsByTagName('body')[0].classname = theme
+    document.getElementsByTagName('main')[0].classname = theme
+    const buttons = document.getElementsByTagName('button')
+
+    for(let i; i < buttons.length; i++) {
+        buttons[i].className = theme
+    }
 }
